dbe5170ddb9b9d864f073d7d3929065b193c0212
Petr Skoda <commits@skodak.org>
2024-09-03 11:05:03 +0200

    remove unused script

--- assets/selenium/debug_forward.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if ! dpkg-query -l socat > dev/null; then
-    sudo apt-get update -qqy && sudo apt-get -qqy --no-install-recommends install socat
-fi
-
-socat TCP-LISTEN:9229,fork TCP4:127.0.0.1:9222
