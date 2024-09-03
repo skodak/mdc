11c37ee591682f71a3721462b86d8b71945edee4
Petr Skoda <commits@skodak.org>
2024-09-03 10:16:09 +0200

    use more consistent mdc progress info

--- assets/db/mariadb_wait.sh
+++ assets/db/mariadb_wait.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo 'Waiting for MariaDB to come up...'
+echo '[mdc] Waiting for MariaDB to come up...'
 until mariadb -h 127.0.0.1 --user=moodle --password="m@0dl3ing" moodle -e "SELECT 'moodle_user_present';" 2> /dev/null| grep -q 'moodle_user_present';
 do
     sleep 1
 done
-echo 'MariaDB is accepting network connections now.'
+echo '[mdc] MariaDB is accepting network connections now.'
 
 
 
